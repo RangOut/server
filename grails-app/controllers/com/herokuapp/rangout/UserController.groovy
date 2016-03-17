3f6b9c6734082277b79224ca0a94aab20e3fa489
Diego Adolfo <diegoado@gmail.com>
2016-03-16 23:51:12 -0300

    Play with grails controllers

--- grails-app/controllers/com/herokuapp/rangout/UserController.groovy
+++ grails-app/controllers/com/herokuapp/rangout/UserController.groovy
@@ -1,21 +1,42 @@
 package com.herokuapp.rangout
 
+import grails.transaction.*
 import grails.converters.JSON
 import grails.rest.RestfulController
-import grails.transaction.Transactional
+
+import org.springframework.dao.DataIntegrityViolationException
 
 @Transactional(readOnly = true)
 class UserController extends RestfulController<User> {
-    
+
+    static allowedMethods = [save: "POST", list: "GET"]
+
     UserController() {
         super(User)
     }
 
     def list() {
-        def users = User.list()
-        render users as JSON
     }
 
+    /**
+     *
+     *
+     * @param oAuth
+     * */
     def save() {
+        // Null-safe access: if params.oAuth is a null value its will be null instead of NullPointerException
+        String oAuth = params?.oAuth
+        def u = new User(oAuth: oAuth)
+
+        try {
+            u.save(flush: true)
+
+            render u as JSON
+            response.setStatus(201)
+            response.setCharacterEncoding("UTF-8")
+            response.setContentType("application/json")
+        } catch(DataIntegrityViolationException e) {
+
+        }
     }
 }
